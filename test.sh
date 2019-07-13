b71050e66fa7f5e514f369ab0c2739169cf44433
ooxi <violetland@mail.ru>
2019-07-13 11:46:02 +0200

    more backends...

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+set -e
 
-make -C8cc clean
-rm -rf 'out'
-make build-$1
-make test-$1
-make test-$1-full
-./tools/check_selfhost.sh $1
+mc $1 make -C8cc clean
+mc $1 rm -rf 'out'
+mc $1 make build-$1
+mc $1 make test-$1
+mc $1 make test-$1-full
+mc $1 ./tools/check_selfhost.sh $1
 
